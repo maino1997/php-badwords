044bf1335e27a14a238adf7b05fd9cfb8997025d
Sasha Mainardi <sashamainardi4@gmail.com>
2022-02-24 18:36:11 +0100

    with variable

--- bad_words.php
+++ bad_words.php
@@ -4,6 +4,8 @@ $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Harum praesentium fugiat magni atque unde, nostrum animi nulla voluptatibus accusantium dolores. 
 Cupiditate enim unde, nesciunt cumque consectetur corrupti magni? Corrupti, dignissimos';
 
+$text_length = strlen($text);
+
 // First method with control 
 $word = $_GET ? $_GET['parola'] : '';
 
@@ -12,6 +14,8 @@ $word = $_GET ? $_GET['parola'] : '';
 // $word = $_GET['parola'];
 
 $new_text = str_replace($word, '***', $text);
+
+$new_text_length = strlen($new_text);
 
 ?>
 
@@ -27,10 +31,16 @@ $new_text = str_replace($word, '***', $text);
 </head>
 <body>
     <h2>Testo non censurato</h2>
-    <p><?php echo$text ?> con lunghezza <?php echo strlen($text) ?></p>
+    <p><?php echo$text ?> con lunghezza <?php echo $text_length ?></p>
     <br>
     <br>
     <h2>Testo con parola <?php echo$word ?> cenruata</h2>
-    <p><?php echo$new_text ?> con lunghezza <?php echo strlen($new_text) ?></p>
+    <p><?php echo$new_text ?> con lunghezza <?php echo $new_text_length ?></p>
+
+
+    <form action="bad_words.php">
+        <input type="text" name="parola">
+        <button type="submit">Invia</button>
+    </form>
 </body>
 </html>
